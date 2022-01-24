e3c3c75b6a582a9d275f16bf8685d264c8c3bfe2
Tobaey <ademolatobie@gmail.com>
2022-01-24 16:40:59 +0100

    init

---
+++ src/pages/NewMeetup.js
@@ -0,0 +1,29 @@
+import { useHistory } from "react-router-dom";
+import NewMeetupForm from "../components/meetups/NewMeetupForm";
+
+function NewMeetupPage() {
+  const history = useHistory();
+  function addMeetupHanler(meetupData) {
+    fetch(
+      "https://react-getting-started-4bdff-default-rtdb.firebaseio.com/meetups.json",
+      {
+        method: "POST",
+        body: JSON.stringify(meetupData),
+        headers: {
+          "Content-Type": "application/json",
+        },
+      }
+    ).then(() => {
+      history.replace("/");
+    });
+  }
+
+  return (
+    <section>
+      <h1>Add New Meetup</h1>
+      <NewMeetupForm onAddMeetup={addMeetupHanler} />
+    </section>
+  );
+}
+
+export default NewMeetupPage;
